b943b721f4cefbdc4790c35832750365177ab6b3
Daniel <danielelielgaio@gmail.com>
2020-07-22 15:58:42 -0300

    Criação das variaveis 'result' e 'count'

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -8,46 +8,51 @@
 
 // Put your code here.
 
-    @R0
+    @R1
     D = M   // D = primeiro operando
+    @result
+    M = D
 
-    @i      // Aloca registrador em algum endereço, com label i. Alocado em posição 16
-    M = D   // memória na posição do registrador alocado recebe primeiro operando. Inicialização do contador
-
-    @R1
-    D = M   // D = segundo operando
-
-    @b      // Criação de um label. é traduzido para um número de registrador. Será alocado em 17
-    M = D   // memória ram na posição b recebe o número b carregado anteriormente
-
-    @c      // label para guardar o resultado. Posição 18
-    M = 0   // inicializa variável que quarda resultado, com valor zero
-
-(LOOP)      // criação de rótulo para branches
-    @i
-    D = M   // register D = M[i]      CONTINUAR .....
-    @n
-    D = D - M // D = D - M[n]
-    @STOP // pula para o endereço rotulado como STOP
-    D ; JGT // if i > n goto STOP
-
-    @sum
+    @R0
     D = M
-    @i
-    D = D + M
-    @sum
-    M = D // sum = sum + i
-    @i
-    M = M + 1 // i = i + 1
-    @LOOP // volta para onde esta o rótulo LOOP
-    0 ; JMP // com esse jump incondicional
 
-(STOP)
-    @sum
-    D = M // carrega resultado da soma para registrador D
-    @R1
-    M = D // RAM[1] = sum. Salva resultado na posição 1 da memória
+    @count
+    M = D
 
-(END) // loop infinito de finalização de programa
-    @END
-    0 ; JMP
+    // CONTINUAR ....
+
+//(LOOP)      // criação de rótulo para branches
+//    @result
+//    M = M + D   // result + R1
+    
+//    @count
+//    M = M + 1
+//    D = M
+
+//    @STOP // pula para o endereço rotulado como STOP
+//    D ; JGT // if i > n goto STOP
+
+
+//    @LOOP // volta para onde esta o rótulo LOOP
+//    0 ; JMP // com esse jump incondicional
+
+//(STOP)
+//    @result
+//    D = M
+//    @R2
+//    M = D
+
+//(END) // loop infinito de finalização de programa
+//    @END
+//    0 ; JMP
+
+
+//    int soma(r0, r1, r2){
+//        a = r0;
+//        b = r1;
+//        count = 0;
+
+//        do{
+//            b = b + b;
+//        }while(count <= a);
+//    }
